7b6183618512426ae188f991063cd2059327d8ea
Bau Sébastien <72702898+sebaucillon@users.noreply.github.com>
2020-10-27 16:09:31 +0100

    Rename single letter variables, vsa, except units as u.

--- astroquery/vsa/tests/test_vista_remote.py
+++ astroquery/vsa/tests/test_vista_remote.py
@@ -5,7 +5,7 @@ import pytest
 
 from astropy.table import Table
 from astropy.coordinates import SkyCoord
-import astropy.units as units
+import astropy.units as u
 
 from ... import vsa
 
@@ -26,41 +26,41 @@ class TestVista:
     @pytest.mark.dependency(depends=["vsa_up"])
     def test_get_images(self):
 
-        crd = SkyCoord(l=336.489, b=-1.48, unit=(units.deg, units.deg), frame='galactic')
+        crd = SkyCoord(l=336.489, b=-1.48, unit=(u.deg, u.deg), frame='galactic')
         images = vista.get_images(crd, frame_type='tilestack',
-                                  image_width=5 * units.arcmin, waveband='H')
+                                  image_width=5 * u.arcmin, waveband='H')
         assert images is not None
 
     @pytest.mark.dependency(depends=["vsa_up"])
     def test_get_images_async(self):
-        crd = SkyCoord(l=336.489, b=-1.48, unit=(units.deg, units.deg), frame='galactic')
+        crd = SkyCoord(l=336.489, b=-1.48, unit=(u.deg, u.deg), frame='galactic')
         images = vista.get_images_async(crd, frame_type='tilestack',
-                                        image_width=5 * units.arcmin, waveband='H')
+                                        image_width=5 * u.arcmin, waveband='H')
         assert images is not None
 
     @pytest.mark.dependency(depends=["vsa_up"])
     def test_get_image_list(self):
-        crd = SkyCoord(l=350.488, b=0.949, unit=(units.deg, units.deg), frame='galactic')
+        crd = SkyCoord(l=350.488, b=0.949, unit=(u.deg, u.deg), frame='galactic')
         urls = vista.get_image_list(crd, frame_type='all', waveband='all')
         assert len(urls) > 0
 
     @pytest.mark.dependency(depends=["vsa_up"])
     def test_query_region_async(self):
-        crd = SkyCoord(l=350.488, b=0.949, unit=(units.deg, units.deg), frame='galactic')
-        response = vista.query_region_async(crd, radius=6 * units.arcsec, programme_id='VVV')
+        crd = SkyCoord(l=350.488, b=0.949, unit=(u.deg, u.deg), frame='galactic')
+        response = vista.query_region_async(crd, radius=6 * u.arcsec, programme_id='VVV')
         assert response is not None
 
     @pytest.mark.dependency(depends=["vsa_up"])
     def test_query_region(self):
-        crd = SkyCoord(l=350.488, b=0.949, unit=(units.deg, units.deg), frame='galactic')
-        table = vista.query_region(crd, radius=6 * units.arcsec, programme_id='VVV')
+        crd = SkyCoord(l=350.488, b=0.949, unit=(u.deg, u.deg), frame='galactic')
+        table = vista.query_region(crd, radius=6 * u.arcsec, programme_id='VVV')
         assert isinstance(table, Table)
         assert len(table) > 0
 
     @pytest.mark.dependency(depends=["vsa_up"])
     def test_query_region_constraints(self):
-        crd = SkyCoord(l=350.488, b=0.949, unit=(units.deg, units.deg), frame='galactic')
-        rad = 6 * units.arcsec
+        crd = SkyCoord(l=350.488, b=0.949, unit=(u.deg, u.deg), frame='galactic')
+        rad = 6 * u.arcsec
         constraints = '(priOrSec<=0 OR priOrSec=frameSetID)'
         table_noconstraint = vista.query_region(crd, radius=rad, programme_id='VVV')
         table_constraint = vista.query_region(crd, radius=rad, programme_id='VVV',
